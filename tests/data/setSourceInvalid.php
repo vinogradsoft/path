0a935f5019f6ecf9d7060185188659d6a183d03f
vinogradsoft <grigorysvetlanin@yandex.ru>
2023-10-12 01:46:14 +0300

    URL parsing fix.

--- tests/data/setSourceInvalid.php
+++ tests/data/setSourceInvalid.php
@@ -6,4 +6,25 @@ return [
     [#1
         'http://',
     ],
+    [#2
+        'path',
+    ],
+    [#3
+        'grigor:password@',
+    ],
+    [#4
+        '.soft:8080',
+    ],
+    [#5
+        '/path/to/resource',
+    ],
+    [#6
+        '#fragment',
+    ],
+    [#7
+        'query=value',
+    ],
+    [#8
+        '?query=value',
+    ]
 ];
